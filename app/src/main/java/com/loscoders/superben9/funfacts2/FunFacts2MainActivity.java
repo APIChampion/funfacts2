1befbf98f181a4912e39ace166d4de981cc4ab95
benrodriguez9 <benrodriguez9@gmail.com>
2017-05-09 20:25:47 -0500

    Initial commit

---
+++ app/src/main/java/com/loscoders/superben9/funfacts2/FunFacts2MainActivity.java
@@ -0,0 +1,13 @@
+package com.loscoders.superben9.funfacts2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FunFacts2MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts2_main);
+    }
+}
