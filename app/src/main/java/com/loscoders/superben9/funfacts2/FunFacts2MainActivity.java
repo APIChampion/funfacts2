46423cfd4ba0daa4b46776696911d92d1a759e72
brodb <brodb@allstate.com>
2017-05-22 12:48:09 -0500

    add on clicklistener

--- app/src/main/java/com/loscoders/superben9/funfacts2/FunFacts2MainActivity.java
+++ app/src/main/java/com/loscoders/superben9/funfacts2/FunFacts2MainActivity.java
@@ -3,6 +3,7 @@ package com.loscoders.superben9.funfacts2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,5 +17,30 @@ public class FunFacts2MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts2_main);
+
+        // assing the views from the layout file to the corresponding variables
+        //android automatically builds a resource file
+
+        mfactTextView = (TextView) findViewById(R.id.FactTextView);
+        mShowFactButton = (Button)findViewById(R.id.ShowFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // the button was clicked so update the t
+                String fact = "Ostriches can run faster than horses";
+                mfactTextView.setText(fact);
+
+
+
+            }
+        };
+
+
+        mShowFactButton.setOnClickListener(listener);
+
+
+
+
     }
 }
